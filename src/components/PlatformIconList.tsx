48dba20d6a600f37e8e709facf231425eb53fb33
singh-manas-ranjan <manassingh900@gmail.com>
2023-08-24 13:59:01 +0530

    Refactor import statement in PlatformIconList

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../services/gameService";
+import { Platform } from "../services/platformService";
 import {
   FaWindows,
   FaLinux,

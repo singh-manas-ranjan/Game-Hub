dd3131fdea02600cacb417e29389dffaf2cf41b1
Manas Ranjan Singh <manassingh900@gmail.com>
2023-09-27 16:51:11 +0530

    Refactor - Navbar icon link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,14 +3,16 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
+import useGameQueryStore from "../store/GameQueryStore";
 
 const NavBar = () => {
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
   return (
     <HStack paddingInline={2} paddingBlock="10px" whiteSpace={"nowrap"}>
       <Link
         to="/"
         style={{ cursor: "pointer", display: "flex", alignItems: "center" }}
-        onClick={() => location.reload()}
+        onClick={() => setSearchText("")}
       >
         <Box boxSize="50px">
           <Image src={logo} />
